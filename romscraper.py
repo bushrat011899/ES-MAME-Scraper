9ad0eb449df51590b0e6bb4e22aaf8aacf28b35f
bushrat011899 <zac@harrold.com.au>
2013-05-28 09:58:30 +1000

    Added Support For Images

--- romscraper.py
+++ romscraper.py
@@ -25,16 +25,28 @@ parser.add_argument('rom', metavar='ROM', type=str, help='a rom file/folder (see
 parser.add_argument('-o', '--output', type=str, help="output file (XML)", required=False, default="gamelist.xml")
 parser.add_argument('-d', '--debug', action='store_true', help="print debug info", required=False)
 parser.add_argument('-p', '--path', action='store_true', help="scrape info from a whole folder", required=False)
+parser.add_argument('-i', '--image', action='store_true', help="retrieve images (when available)", required=False)
 args            = parser.parse_args()
 
 whole_file      = args.rom
 debug_mode      = args.debug
 folder_mode     = args.path
+image_mode		= args.image
 output          = args.output
 
 def debug_print(string):
         if debug_mode:
                 print string
+
+def test_url(url):
+	if has_requests:
+        try:
+            r = requests.get(url)
+            r.raise_for_status()
+        except HTTPError:
+        	return False
+        else:
+        	return True
 
 debug_print(os.path.abspath(__file__))
 debug_print(os.path.abspath(whole_file)) 
@@ -61,32 +73,18 @@ for i in xrange(len(files)):
     debug_print("Path  : " + path)
     debug_print("File  : " + file)
     debug_print("ROM   : " + rom)
-
-    if has_requests:
-        debug_print("Has Requests.......")
-        debug_print("Using Try/Except...")
-        try:
-            r = requests.get('http://mamedb.com/game/'+ rom)
-            r.raise_for_status()
-        except HTTPError:
-            debug_print('Could not download page')
-            skip = True
-        else:
-            req = urllib2.Request('http://mamedb.com/game/'+ rom)
-            response = urllib2.urlopen(req)
-            debug_print('Downloaded Successfully')
-            html = response.read()
-            response.close()
-            skip = False
-    else:
-        debug_print("Does NOT Have Requests.....")
-        debug_print("Going Straight to Urllib...")
+	
+	
+	if test_url('http://mamedb.com/game/'+ rom):
         req = urllib2.Request('http://mamedb.com/game/'+ rom)
         response = urllib2.urlopen(req)
         debug_print('Downloaded Successfully')
         html = response.read()
         response.close()
-        skip = False
+    	skip = False
+    else:
+    	debug_print('Download Failed')
+    	skip = True	
 
     if skip == False:
         debug_print("Filtering Results...")
@@ -113,6 +111,47 @@ for i in xrange(len(files)):
 		debug_print("Found Manufacturer: " + manu)
 	else:
 		debug_print("No Results Found")
+		
+	### Experimental Image Getting Code Start
+	if image_mode:
+		if test_url("http://mamedb.com/snap/" + rom + ".png"):
+			urllib.urlretrieve("http://mamedb.com/snap/" + rom + ".png",  "images/snap/"+ rom + "_snap" + ".png")
+			snap	= "images/snap/"+ rom + "_snap" + ".png"
+			debug_print("Found Snap Image")
+		else:
+			snap 	= None
+			debug_print("No Snap Image Found")
+	
+		if test_url("http://mamedb.com/titles/" + rom + ".png"):
+			urllib.urlretrieve("http://mamedb.com/titles/" + rom + ".png",  "images/title/"+ rom + "_title" + ".png")
+			title	= "images/title/"+ rom + "_title" + ".png"
+			debug_print("Found Title Image")
+		else:
+			title	= None
+			debug_print("No Title Image Found")
+	
+		if test_url("http://mamedb.com/cabinets/" + rom + ".png"):
+			urllib.urlretrieve("http://mamedb.com/cabinets/" + rom + ".png",  "images/cabinet/"+ rom + "_cabinet" + ".png")
+			cabinet	= "images/cabinet/"+ rom + "_cabinet" + ".png"
+			debug_print("Found Cabinet Image")
+		else:
+			cabinet	= None
+			debug_print("No Cabinet Image Found")
+		
+		if test_url("http://mamedb.com/marquees/" + rom + ".png"):
+			urllib.urlretrieve("http://mamedb.com/marquees/" + rom + ".png",  "images/marquee/"+ rom + "_marquee" + ".png")
+			marquee	= "images/marquee/"+ rom + "_marquee" + ".png"
+			debug_print("Found Marquee Image")
+		else:
+			marquee	= None
+			debug_print("No Marquee Image Found")
+	else:
+		snap 	= None
+		title	= None
+		cabinet	= None
+		marquee	= None
+		
+	### End
 
 	if has_elementtree:
             skip_node = False
@@ -122,21 +161,21 @@ for i in xrange(len(files)):
                         skip_node = True
            
             if skip_node == False:
-		print "Found " + path
+				print "Found " + path
                 print "Adding " + name
-		print "From Year:	" + year
-		print "Made By:	" + manu
-                debug_print("Done")
+				print "From Year:	" + year
+				print "Made By:	" + manu
+        		debug_print("Done")
 
-                xml_game        = et.SubElement(xml_root, "game")
-                xml_path        = et.SubElement(xml_game, "path")
-                xml_path.text   = path
-                xml_name        = et.SubElement(xml_game, "name")
-                xml_name.text   = name
-		xml_year	= et.SubElement(xml_game, "year")
-		xml_year.text	= year
-		xml_manu	= et.SubElement(xml_game, "manufacturer")
-		xml_manu.text	= manu  
+       		 	xml_game        = et.SubElement(xml_root, "game")
+       		 	xml_path        = et.SubElement(xml_game, "path")
+        		xml_path.text   = path
+        		xml_name        = et.SubElement(xml_game, "name")
+        		xml_name.text   = name
+				xml_year	= et.SubElement(xml_game, "year")
+				xml_year.text	= year
+				xml_manu	= et.SubElement(xml_game, "manufacturer")
+				xml_manu.text	= manu
             else:
                 print "Skipping " + rom
     else:
